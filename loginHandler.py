10d77ca4e97c99ba42aa76906716d24b7e6ed265
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-03 15:18:37 +0800

    Mostly working LoginHandler writing routines. Just need to reset the xml to correct format and code adding new entries.

--- loginHandler.py
+++ loginHandler.py
@@ -2,7 +2,7 @@ from xml.dom import minidom
 import xml.etree.ElementTree as ET
 import os, platform, numpy, datetime
 cwd = os.path.dirname(os.path.realpath(__file__))#take note this doesnt work when os.chdir() is called!
-sett = open(cwd + '/logins.xml', 'r+')
+sett = open(cwd + '/logins.xml', 'r')
 settings = ET.parse(sett)
 root = settings.getroot()
 setArray = numpy.empty((30), dtype=object)
@@ -11,18 +11,30 @@ def isLogin(fgt):
     now = datetime.datetime.utcnow()
     nowString = now.strftime("%m/%d/%Y %H:%M")
     x = 0
-    for element in root:
+    for element in root.findall('login'):
         try:
             c = root[x].attrib[fgt]
             x +=1
         except KeyError:
             pass
         if (datetime.datetime.strptime(root[x].attrib['expires'], '%m/%d/%Y %H:%M') < now):
-            return False
+            print(False)
 def newLogin(fgt):
-    print("Write login here")
-##    now = datetime.datetime.utcnow()
-##    nowString = now.strftime("%m/%d/%Y %H:%M")
+    now = datetime.datetime.utcnow()
+    nowString = now.strftime("%m/%d/%Y %H:%M")
+    x = 0
+    for element in root.findall('login'):
+        try:
+            c = root[x].attrib[fgt]
+            x +=1
+        except KeyError:
+            pass
+        root[x].set('expires', 'never')
+        settw = open(cwd + '/logins.xml', 'wb')
+        settings.write(settw)
+        settw.close()
+        ##if (datetime.datetime.strptime(root[x].attrib['expires'], '%m/%d/%Y %H:%M') < now):
+##            print(False)
 ##    x = 0
 ##    for element in root:
 ##        try:
