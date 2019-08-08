0ebd8512d57da63a9b1c7c99afb0480d2400f915
Jericho Rejuso <clevergold6@gmail.com>
2019-08-09 07:55:07 +0800

    Threaded voltagegen

--- voltagegen.py
+++ voltagegen.py
@@ -1,14 +1,21 @@
-import time, math, os
+import time, math, os, threading, queue
 cwd = os.path.dirname(os.path.realpath(__file__))#take note this doesnt work when os.chdir() is called!
-y = x = 0
-##file = open(cwd + '/etits.txt', 'r+')
-while True:
-    time.sleep(0.0166666666667)
-    x += 0.0166666666667
-    y = math.sin(x)
-    v = 325.2691193 * y
+measurements = queue.Queue()
+def measure():
+    y = x = 0
+    ##file = open(cwd + '/etits.txt', 'r+')
+    while True:
+        time.sleep(0.0166666666667)
+        x += 0.0166666666667
+        y = math.sin(x)
+        v = 325.2691193 * y
+        measurements.put(str(v))
 ##    file = open(cwd + '/etits.txt', 'a')
 ##    file.write(str(v)+'\n')
 ##    file.close()
-    print (v)
-    return(v)
+class thread (threading.Thread):
+    def run(self):
+        measure()
+measThread = thread()
+measThread.start()
+
