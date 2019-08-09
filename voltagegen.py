d364b379a8c0bece24153185e2750fb5546bc4d3
Jericho Rejuso <clevergold6@gmail.com>
2019-08-09 13:25:24 +0800

    Voltage value generator probably works nao

--- voltagegen.py
+++ voltagegen.py
@@ -1,6 +1,14 @@
 import time, math, os, threading, queue, numpy
 cwd = os.path.dirname(os.path.realpath(__file__))#take note this doesnt work when os.chdir() is called!
-measurements = queue.Queue()
+measurements = queue.Queue(120)
+def peakVoltage():
+        measarray = []
+        if (measurements.full()):
+            while (not measurements.empty()):
+##                print(measurements.qsize())
+                measarray.append(measurements.get())
+                measurements.task_done()
+            return(max(measarray)/math.sqrt(2))
 def measure():
     x = 0
     ##file = open(cwd + '/etits.txt', 'r+')
@@ -9,17 +17,18 @@ def measure():
         x += 0.0166666666667
         y = math.sin(x)
         v = 325.2691193 * y
-        measurements.put(str(v))
-        if (x%1 !=0):
+        measurements.put(v)
+        if (x%1 == 0):
             measurements.join()
+        if (measurements.full()):
+            return(peakVoltage())
+            break
 ##    file = open(cwd + '/etits.txt', 'a')
 ##    file.write(str(v)+'\n')
 ##    file.close()
-class thread (threading.Thread):
-    def run(self):
-        measure()
-measThread = thread()
-measThread.start()
-while True:
-    print(max(measurements.get()))
-    measurements.task_done()
+##measThread = thread()
+##measThread.start()
+
+
+        
+        
