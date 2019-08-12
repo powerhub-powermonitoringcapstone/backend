15d358d465b97e2b8a9a5f9b9c6eca809a616a04
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-12 18:36:49 +0800

    Fixed loginhandler bug which lead it to not see login.xml updates... Also in the process of implementing asynchronous HTTP methods for data updates in main.html

--- loginHandler.py
+++ loginHandler.py
@@ -2,45 +2,54 @@ from xml.dom import minidom
 import xml.etree.ElementTree as ET
 import os, platform, numpy, datetime
 cwd = os.path.dirname(os.path.realpath(__file__))#take note this doesnt work when os.chdir() is called!
-sett = open(cwd + '/logins.xml', 'r')
-settings = ET.parse(sett)
-root = settings.getroot()
 setArray = numpy.empty((30), dtype=object)
-setWarray = numpy.empty((30), dtype=object)
-def Write():
-    settw = open (cwd + '/logins.xml', 'wb')
-    settings.write(settw)
-    settw.close()
+setWarray = numpy.empty((30), dtype=object)    
 def isLogin(fgt):
-    fgt = str(fgt)
-    now = datetime.datetime.utcnow()
-    nowString = now.strftime("%m/%d/%Y %H:%M")
-    found = root.find(".//logind/[@fgt={}]".format("\""+fgt+"\""))
-    if (found == None):
-        return False
-    else:
-        if (datetime.datetime.strptime(found.attrib['expires'], '%m/%d/%Y %H:%M') < now):
-            root.remove(found)
-            Write()
-            return(False)
+    with open(cwd + '/logins.xml', 'r') as sett:
+        settings = ET.parse(sett)
+        root = settings.getroot()
+        fgt = str(fgt)
+        now = datetime.datetime.utcnow()
+        nowString = now.strftime("%m/%d/%Y %H:%M")
+        found = root.find(".//logind/[@fgt={}]".format("\""+fgt+"\""))
+        if (found == None):
+            return False
         else:
-            return(True)
+            if (datetime.datetime.strptime(found.attrib['expires'], '%m/%d/%Y %H:%M') < now):
+                root.remove(found)
+                Write()
+                return(False)
+            else:
+                return(True)
+        settw = open (cwd + '/logins.xml', 'wb')
+        settings.write(settw)
+        settw.close()
 def newLogin(fgt):
-    fgt = str(fgt)
-    mntnLogin()
-    now = datetime.datetime.utcnow()
-    nowString = (now + datetime.timedelta(minutes = 10)).strftime("%m/%d/%Y %H:%M")
-    found = root.find(".//logind/[@fgt={}]".format("\""+fgt+"\""))
-    if (found == None):
-        root.append(ET.Element("logind", {'expires': nowString, 'fgt': fgt, 'interval':"10"}))
-    else:
-        found.set('expires', nowString)
-    Write()
+    with open(cwd + '/logins.xml', 'r') as sett:
+        settings = ET.parse(sett)
+        root = settings.getroot()
+        fgt = str(fgt)
+        mntnLogin()
+        now = datetime.datetime.utcnow()
+        nowString = (now + datetime.timedelta(minutes = 10)).strftime("%m/%d/%Y %H:%M")
+        found = root.find(".//logind/[@fgt={}]".format("\""+fgt+"\""))
+        if (found == None):
+            root.append(ET.Element("logind", {'expires': nowString, 'fgt': fgt, 'interval':"10"}))
+        else:
+            found.set('expires', nowString)
+        settw = open (cwd + '/logins.xml', 'wb')
+        settings.write(settw)
+        settw.close()
 def mntnLogin():
-    found = root.findall(".logind")[19:]
-    for elem in found:
-        root.remove(elem)
-    Write()
+    with open(cwd + '/logins.xml', 'r') as sett:
+        settings = ET.parse(sett)
+        root = settings.getroot()
+        found = root.findall(".logind")[19:]
+        for elem in found:
+            root.remove(elem)
+        settw = open (cwd + '/logins.xml', 'wb')
+        settings.write(settw)
+        settw.close()
     
     
                 
