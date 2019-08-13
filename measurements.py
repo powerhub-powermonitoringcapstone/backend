bfcf7f23ad27d842f31ba07d472915b2861a357c
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-13 20:34:25 +0800

    Lazy code merge with voltagegen.py in preparation for better thread handling (measurement saving subprogram)

--- measurements.py
+++ measurements.py
@@ -1,7 +1,31 @@
 import xml.etree.ElementTree as ET
-import os, platform, numpy, datetime
+import os, platform, numpy, datetime, time, threading, queue, math
 cwd = os.path.dirname(os.path.realpath(__file__))
+rawdata = queue.Queue(360)
+
+##implement as separate thread
+def peakVoltage():
+    measarray = []
+    if (rawdata.full()):
+        while (not rawdata.empty()):
+            measarray.append(rawdata.get())
+            rawdata.task_done()
+        return(max(measarray)/math.sqrt(2), min(measarray)/math.sqrt(2))
+def measure():
+    x = 0
+    while True:
+        time.sleep(0.0166666666667)
+        x += (0.0166666666667)
+        y = math.sin(x)
+        v = 325.2691193 * y
+        rawdata.put(v)
+##        if (x%2 == 0):
+##            rawdata.join()
+        if (rawdata.full()):
+            return(peakVoltage())
+            break
 ##main loop
+
 try:
     sett = open(cwd+'/measurements.xml', 'r')
     sett.close()
