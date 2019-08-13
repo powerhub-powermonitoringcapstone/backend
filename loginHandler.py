8953f12a5f677c3a53e04fd74e938e214770e1a9
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-13 18:08:48 +0800

    Reworked login system

--- loginHandler.py
+++ loginHandler.py
@@ -3,43 +3,7 @@ import xml.etree.ElementTree as ET
 import os, platform, numpy, datetime
 cwd = os.path.dirname(os.path.realpath(__file__))#take note this doesnt work when os.chdir() is called!
 setArray = numpy.empty((30), dtype=object)
-setWarray = numpy.empty((30), dtype=object)    
-def isLogin(fgt):
-    with open(cwd + '/logins.xml', 'r') as sett:
-        settings = ET.parse(sett)
-        root = settings.getroot()
-        fgt = str(fgt)
-        now = datetime.datetime.utcnow()
-        nowString = now.strftime("%m/%d/%Y %H:%M")
-        found = root.find(".//logind/[@fgt={}]".format("\""+fgt+"\""))
-        if (found == None):
-            return False
-        else:
-            if (datetime.datetime.strptime(found.attrib['expires'], '%m/%d/%Y %H:%M') < now):
-                root.remove(found)
-                Write()
-                return(False)
-            else:
-                return(True)
-        settw = open (cwd + '/logins.xml', 'wb')
-        settings.write(settw)
-        settw.close()
-def newLogin(fgt):
-    with open(cwd + '/logins.xml', 'r') as sett:
-        settings = ET.parse(sett)
-        root = settings.getroot()
-        fgt = str(fgt)
-        mntnLogin()
-        now = datetime.datetime.utcnow()
-        nowString = (now + datetime.timedelta(minutes = 10)).strftime("%m/%d/%Y %H:%M")
-        found = root.find(".//logind/[@fgt={}]".format("\""+fgt+"\""))
-        if (found == None):
-            root.append(ET.Element("logind", {'expires': nowString, 'fgt': fgt, 'interval':"10"}))
-        else:
-            found.set('expires', nowString)
-        settw = open (cwd + '/logins.xml', 'wb')
-        settings.write(settw)
-        settw.close()
+setWarray = numpy.empty((30), dtype=object)
 def mntnLogin():
     with open(cwd + '/logins.xml', 'r') as sett:
         settings = ET.parse(sett)
@@ -50,6 +14,44 @@ def mntnLogin():
         settw = open (cwd + '/logins.xml', 'wb')
         settings.write(settw)
         settw.close()
+def isLogin(fgt):
+    mntnLogin()
+    with open(cwd + '/logins.xml', 'r') as sett:
+        settings = ET.parse(sett)
+        root = settings.getroot()
+        fgt = str(fgt)
+        now = datetime.datetime.utcnow()
+        nowString = now.strftime("%m/%d/%Y %H:%M")
+        found = root.find(".//logind/[@fgt={}]".format("\""+fgt+"\""))
+        if (found == None):
+            return False
+        else:
+            return True
+##            if (True):datetime.datetime.strptime(found.attrib['expires'], '%m/%d/%Y %H:%M') < now):
+##                root.remove(found)
+##                return(False)
+##            else:
+##                return(True)
+        settw = open (cwd + '/logins.xml', 'wb')
+        settings.write(settw)
+        settw.close()
+def newLogin(fgt):
+    mntnLogin()
+    with open(cwd + '/logins.xml', 'r') as sett:
+        settings = ET.parse(sett)
+        root = settings.getroot()
+        fgt = str(fgt)
+        now = datetime.datetime.utcnow()
+        nowString = (now + datetime.timedelta(minutes = 10)).strftime("%m/%d/%Y %H:%M")
+        found = root.find(".//logind/[@fgt={}]".format("\""+fgt+"\""))
+        if (found == None):
+            root.append(ET.Element("logind", {'fgt': fgt}))#{'expires': nowString, 'fgt': fgt, 'interval':"10"}))
+        else:
+            found.set('expires', nowString)
+        settw = open (cwd + '/logins.xml', 'wb')
+        settings.write(settw)
+        settw.close()
+
     
     
                 
