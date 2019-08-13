db68b036e235a4607c40fe40799ae06769c44978
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-13 19:35:59 +0800

    Commented out currently unused loginhandler calls

--- loginHandler.py
+++ loginHandler.py
@@ -20,8 +20,8 @@ def isLogin(fgt):
         settings = ET.parse(sett)
         root = settings.getroot()
         fgt = str(fgt)
-        now = datetime.datetime.utcnow()
-        nowString = now.strftime("%m/%d/%Y %H:%M")
+##        now = datetime.datetime.utcnow()
+##        nowString = now.strftime("%m/%d/%Y %H:%M")
         found = root.find(".//logind/[@fgt={}]".format("\""+fgt+"\""))
         if (found == None):
             return False
@@ -41,13 +41,13 @@ def newLogin(fgt):
         settings = ET.parse(sett)
         root = settings.getroot()
         fgt = str(fgt)
-        now = datetime.datetime.utcnow()
-        nowString = (now + datetime.timedelta(minutes = 10)).strftime("%m/%d/%Y %H:%M")
+##        now = datetime.datetime.utcnow()
+##        nowString = (now + datetime.timedelta(minutes = 10)).strftime("%m/%d/%Y %H:%M")
         found = root.find(".//logind/[@fgt={}]".format("\""+fgt+"\""))
         if (found == None):
             root.append(ET.Element("logind", {'fgt': fgt}))#{'expires': nowString, 'fgt': fgt, 'interval':"10"}))
         else:
-            found.set('expires', nowString)
+            found.set('fgt', fgt)
         settw = open (cwd + '/logins.xml', 'wb')
         settings.write(settw)
         settw.close()
