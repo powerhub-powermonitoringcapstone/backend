f7298221489b0ea40f0aaf843f6b81b48da4f6be
Jericho Rejuso <clevergold6@gmail.com>
2019-08-13 16:18:41 +0800

    random voltage generator fix na ewan ko ginawa ko

--- voltagegen.py
+++ voltagegen.py
@@ -1,25 +1,25 @@
 import time, math, os, threading, queue, numpy
 cwd = os.path.dirname(os.path.realpath(__file__))#take note this doesnt work when os.chdir() is called!
-measurements = queue.Queue(120)
+measurements = queue.Queue(360)
 def peakVoltage():
-        measarray = []
-        if (measurements.full()):
-            while (not measurements.empty()):
+    measarray = []
+    if (measurements.full()):
+        while (not measurements.empty()):
 ##                print(measurements.qsize())
-                measarray.append(measurements.get())
-                measurements.task_done()
-            return(max(measarray)/math.sqrt(2))
+            measarray.append(measurements.get())
+            measurements.task_done()
+        return(max(measarray)/math.sqrt(2), min(measarray)/math.sqrt(2))
 def measure():
     x = 0
     ##file = open(cwd + '/etits.txt', 'r+')
     while True:
         time.sleep(0.0166666666667)
-        x += 0.0166666666667
+        x += (0.0166666666667)
         y = math.sin(x)
         v = 325.2691193 * y
         measurements.put(v)
-        if (x%1 == 0):
-            measurements.join()
+##        if (x%2 == 0):
+##            measurements.join()
         if (measurements.full()):
             return(peakVoltage())
             break
