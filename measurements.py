fa7a01bb1eea840aaf75a9c530ad6f0a48c3c3bb
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-08-21 13:26:49 +0800

    Password back to powerhub, gearing up for the generation of database files from dummy data and all the related graphing stuff

--- measurements.py
+++ measurements.py
@@ -1,31 +1,24 @@
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET 
 import os, platform, numpy, datetime, time, threading, queue, math
 cwd = os.path.dirname(os.path.realpath(__file__))
 rawdata = queue.Queue(360)
 
 ##implement as separate thread
-def peakVoltage():
-    measarray = []
-    if (rawdata.full()):
-        while (not rawdata.empty()):
-            measarray.append(rawdata.get())
-            rawdata.task_done()
-        return(max(measarray)/math.sqrt(2), min(measarray)/math.sqrt(2))
-def measure():
-    x = 0
+def data():
     while True:
-        time.sleep(0.0166666666667)
-        x += (0.0166666666667)
-        y = math.sin(x)
-        v = 325.2691193 * y
-        rawdata.put(v)
-##        if (x%2 == 0):
-##            rawdata.join()
-        if (rawdata.full()):
-            return(peakVoltage())
-            break
+        with open(cwd+'/data.xml', 'r') as sett:
+            stuff = ET.parse(sett)
+            root = stuff.getroot()
+            for element in root.findall("./dummy"):
+                rawdata.put({"voltage": element.attrib['voltage'],\
+                             "current":element.attrib['current'],\
+                             "n": element.text\
+                             })
+        rawdata.join()
+        time.sleep(0.5)
+msThread = threading.Thread(target=data)
+msThread.start()
 ##main loop
-
 try:
     sett = open(cwd+'/measurements.xml', 'r')
     sett.close()
@@ -33,12 +26,15 @@ except IOError:
     sett = open(cwd+'/measurements.xml', 'w')
     sett.write("<measurements></measurements>")
     sett.close()
-    
-with open(cwd+'/measurements.xml', 'r') as sett:
-    now = datetime.datetime.now()
-    measurements = ET.parse(sett)
-    root = measurements.getroot()
-    root.append(ET.Element("plot",{'voltage':str(230),'current':str(5), 'variation':str(30), 'date': now.strftime("%m/%d/%Y %H:%M:%S")}))
-    settw = open (cwd + '/measurements.xml', 'wb')
-    measurements.write(settw)
-    settw.close()
+
+while True:
+    with open(cwd+'/measurements.xml', 'r') as sett:
+        now = datetime.datetime.now()
+        msFile = ET.parse(sett)
+        root = msFile.getroot()
+        msData = rawdata.get()
+        rawdata.task_done()
+        root.append(ET.Element("plot",{'voltage':str(msData["voltage"]),'current':str(msData["current"]), 'variation':str(30), 'date': now.strftime("%m/%d/%Y %H:%M:%S")}))
+        settw = open (cwd + '/measurements.xml', 'wb')
+        msFile.write(settw)
+        settw.close()
