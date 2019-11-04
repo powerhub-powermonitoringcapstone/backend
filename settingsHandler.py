450da419cce2d1764209dfac8644f8483f1ddcc2
Khalil Ubalde <wrcsstudiokhalil@gmail.com>
2019-11-05 03:15:44 +0800

    Code optimizations, implemented refresh rate parsing

--- settingsHandler.py
+++ settingsHandler.py
@@ -4,6 +4,9 @@ import os, platform, numpy
 cwd = os.path.dirname(os.path.realpath(__file__))
 setArray = numpy.empty((30), dtype=object)
 setWarray = numpy.empty((30), dtype=object)
+d = {'IsSetup':0,'DataLogging':1,'SensitivityThreshold':2\
+     ,'Debug':3, 'NodeName':4, 'Version':5 , 'NodeType':6,\
+     'Permanence':7, 'CarbonFootprint':8, 'RefreshRate':9}
 try:
     sett = open(cwd+'/settings.xml', 'r')
     sett.close()
@@ -13,9 +16,6 @@ except IOError:
         sett.close()
 def riteSettings(f, g): ##index number, pamalit na value
     with open(cwd + '/settings.xml', 'r') as sett:
-        d = {'IsSetup':0,'DataLogging':1,'SensitivityThreshold':2\
-             ,'Debug':3, 'NodeName':4, 'Version':5 , 'NodeType':6,\
-             'Permanence':7, 'CarbonFootprint':8}
         settings = ET.parse(sett)
         root = settings.getroot()
         x = 0
@@ -32,9 +32,7 @@ def readSettings():
         settings = ET.parse(sett)
         root = settings.getroot()
         x = 0
-        d = {'IsSetup':0,'DataLogging':1,'SensitivityThreshold':2,'Debug':3, 'NodeName':4,'Version':5,'NodeType':6,'Permanence':7,'CarbonFootprint':8}
         for element in root:
-            print(element.attrib['name'])
             try:
                 c = d[root[x].attrib['name']]
                 setArray[c] = root[x].text
